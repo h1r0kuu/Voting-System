11d70f684b0b66679af561f969a70bb944044d2d
h1r0kuu <abhazim.nurbat@gmail.com>
2024-01-10 16:58:34 +0100

    feat: add votedetailview and refactor context for votelistview
    Co-Authored-By: Evaletik <104141950+Evaletik@users.noreply.github.com>

--- vote/views.py
+++ vote/views.py
@@ -1,16 +1,26 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import *
+from django.utils import timezone
 
 # Create your views here.
 class VoteListView(ListView):
-    model = Vote
-    template_name = 'votes/vote_list.html'
-    context_object_name = 'votes'
+    model = Voting
+    template_name = 'votings/votings_list.html'
+    context_object_name = 'votings'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         qs = super().get_queryset() 
-        context['expired'] = qs.filter(expired = True)
-        context['current'] = qs.filter(expired = False)
+        now = timezone.now()
+        context['expired'] = qs.filter(end_time__lte = now)
+        context['current'] = qs.filter(end_time__gte = now)
         return context
+
+
+class VoteDetailView(DetailView):
+    model = Voting
+    template_name = 'votings/voting_detail.html'
+    context_object_name = 'voting'
