222aa455dc2bc45a450e499d9d80840bfd865b73
h1r0kuu <abhazim.nurbat@gmail.com>
2024-01-13 19:28:33 +0100

    feat: add new voting list views for finished, current and upcoming votings
    Co-Authored-By: Evaletik <104141950+Evaletik@users.noreply.github.com>

--- vote/urls.py
+++ vote/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', VoteListView.as_view(), name = "home"),
-    path('vote/<int:pk>', VoteDetailView.as_view(), name = "vote_details")
+    path('', CurrentVotingsListView.as_view(), name = "home"),
+    path('vote/<int:pk>', VoteDetailView.as_view(), name = "vote_details"),
+    path('votings/upcoming', UpcomingVotingsListView.as_view(), name = 'upcoming_votings'),
+    path('votings/current', CurrentVotingsListView.as_view(), name = 'current_votings'),
+    path('votings/finished', FinishedVotingsListView.as_view(), name = 'finished_votings'),
 ]
--- vote/views.py
+++ vote/views.py
@@ -4,23 +4,53 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import *
 from django.utils import timezone
+from django.http import JsonResponse
 
-# Create your views here.
-class VoteListView(ListView):
+
+class FinishedVotingsListView(ListView):
     model = Voting
     template_name = 'votings/votings_list.html'
     context_object_name = 'votings'
 
-    def get_context_data(self, **kwargs):
+    def get_queryset(self) -> QuerySet[Any]:
+        return Voting.objects.filter(end_time__lte = timezone.now())
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        qs = super().get_queryset() 
-        now = timezone.now()
-        context['expired'] = qs.filter(end_time__lte = now)
-        context['current'] = qs.filter(end_time__gte = now)
+        context['page_title'] = "Finished votings"
         return context
+
+class CurrentVotingsListView(ListView):
+    model = Voting
+    template_name = 'votings/votings_list.html'
+    context_object_name = 'votings'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        now = timezone.now()
+        return Voting.objects.filter(end_time__gte = now, start_time__lte = now)
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = "Current votings"
+        return context
+
+
+class UpcomingVotingsListView(ListView):
+    model = Voting
+    template_name = 'votings/votings_list.html'
+    context_object_name = 'votings'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Voting.objects.filter(start_time__gte = timezone.now())
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = "Upcoming votings"
+        return context
+
 
 
 class VoteDetailView(DetailView):
     model = Voting
     template_name = 'votings/voting_detail.html'
-    context_object_name = 'voting'
+    context_object_name = 'voting'    context_object_name = 'voting'
