dd264cf5b6e510c2529d61b54169a8c0c98fd1c9
h1r0kuu <abhazim.nurbat@gmail.com>
2024-01-16 22:21:08 +0100

    feat: rangefilter app
    Co-Authored-By: Evaletik <104141950+Evaletik@users.noreply.github.com>

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
 
+    'rangefilter',
+
     'authentication.apps.AuthenticationConfig',
     'vote.apps.VoteConfig',
 ]
